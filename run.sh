14a654748931979f7512518431dbb6e49049fe85
Hunter Perrin <hperrin@gmail.com>
2018-05-07 20:32:16 -0700

    Remove unnecessary db password generation.

--- run.sh
+++ run.sh
@@ -8,11 +8,6 @@ if [ ! -d "app/node_modules" ]; then
   ./npm.sh install
 fi
 
-if [ ! -f "db_password.txt" ]; then
-  dd if=/dev/urandom bs=64 count=1 | base64 > ./db_password.txt
-  dd if=/dev/urandom bs=64 count=1 | base64 > ./db_root_password.txt
-fi
-
 if [ ! -d "db_data" ]; then
   mkdir db_data
 fi
